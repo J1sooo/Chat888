6c462348b8b74f23ba085665d36c9403291554c4
j1sooo <kjisoo1031@gmail.com>
2024-12-14 08:56:24 +0900

    feat: 사용자 로그인 컨트롤러에 CORS 설정 추가

--- back/src/main/java/col/carrot/back/user/userlogin/controller/UserRestController.java
+++ back/src/main/java/col/carrot/back/user/userlogin/controller/UserRestController.java
@@ -11,15 +11,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserRestController {
